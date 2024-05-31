3d671e4001ceca65f05469cc69bd2aaf29a90b0f
monciego <jerichobantiquete@gmail.com>
2024-05-31 15:34:35 +0800

    chore: add remix development tools

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
-import { vitePlugin as remix } from "@remix-run/dev";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { vitePlugin as remix } from '@remix-run/dev';
+import { defineConfig } from 'vite';
+import { remixDevTools } from 'remix-development-tools';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [
+    remixDevTools(),
     remix({
       future: {
         v3_fetcherPersist: true,
